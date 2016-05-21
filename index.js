4642e45723029f661c81cc77b9b80aa856adbc39
Steven Pérez <stevenperez@Stevens-MacBook-Pro.local>
2016-05-21 14:33:39 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const mentors = {
+  gaboesquivel: require('gaboesquivel'),
+  yeco: require('yeco'),
+  brolag: require('brolag'),
+  kevinblanco: require('kevinblanco'),
+  jeanlescure: require('jeanlescure'),
+  laurensortiz: require('laurensortiz'),
+  jarias: require('jarias'),
+  rubenabix: require('rubenabix'),
+  dmunguia: require('dmunguia'),
+  credondocr: require('credondocr'),
+  avenidanet: require('avenidanet'),
+  gmmendezp: require('gmmendezp'),
+  ledezman: require('ledezman'),
+  quiaro: require('quiaro'),
+  cmonge: require('cmonge'),
+  carlosv: require('carlosv'),
+  argosbass: require('argosbass'),
+  gelopfalcon: require('gelopfalcon'),
+  jasancheg: require('jasancheg'),
+  jmonterroso: require('jmonterroso'),
+  jperez: require('jperez'),
+  esramru: require('esramru'),
+  msolano00: require('msolano00'),
+  agarcia: require('agarcia'),
+  jonobral: require('jonobral'),
+  stevenperez: require('stevenperez')
+}
+
+module.exports = mentors
